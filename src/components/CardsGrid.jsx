0013a57a093e3420cac90879af2426b7b6f9803b
jalejo <ja.alejo@gmail.com>
2024-01-25 15:30:50 -0300

    Migrate State Variables / Grid Cards Flipping and matching

--- src/components/CardsGrid.jsx
+++ src/components/CardsGrid.jsx
@@ -1,65 +1,25 @@
-import { useEffect, useState } from "react";
 import Card from "./card";
 
-const animalsCardsUrl = "https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20"
-
-const CardsGrid = () => {
-
-    const [animalsList, setAnimalsList] = useState([]);
-    useEffect(() => {
-        const getCards = async () => {
-            try {
-                const response = await fetch( animalsCardsUrl, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                } );
-
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-
-                const jsonAnimalsData = await response.json();
-
-                const randomAnimals = getRandomAnimals( jsonAnimalsData.entries, 9 );
-
-                const joinedArrays = [...randomAnimals, ...randomAnimals];
-                
-                const gridAnimals = getRandomAnimals( joinedArrays, 18 )
-
-                setAnimalsList( gridAnimals ); 
-
-            } catch ( error ){
-                console.error("Error getting cards:", error );
-            }
-        };
-    
-        getCards();
-    },[] )    
-
-
-    const getRandomAnimals = ( arr, num ) => {
-        const shuffledAnimals = arr.sort(() => 0.5 - Math.random() );
-        return shuffledAnimals.slice( 0, num );
-    };
+const CardsGrid = ({ animalsList, handleCard }) => {
 
     return(
-        <div>
-            <div className="grid grid-cols-6">
-            {
-                animalsList.map((animal, index) => (
-                    <div key={`${animal.fields.image.uuid}-${index}`}>
-                        {animal.fields.image.title}
-                        <img 
-                            src={animal.fields.image.url}
-                            alt={animal.fields.image.title}
-                        />
-                    </div>
-                ))
-            }
-            </div>
+       
+        <div 
+            className="
+                w-full max-w-80 sm:max-w-5xl mx-auto 
+                grid grid-cols-3 sm:grid-cols-6 gap-2 sm:gap-4  
+            ">
+        {
+            animalsList.map((animal, index) => (
+                <Card 
+                    key={`${animal.uuid}-${index}`}
+                    animal = { animal }
+                    handleCard = { handleCard } 
+                />
+            ))
+        }
         </div>
+
     )
 }
 
