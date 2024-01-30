2f61d8473a56cb2c52807322b24b6e6d2d035ea9
jalejo <ja.alejo@gmail.com>
2024-01-30 10:13:46 -0300

    mobile view updates

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ function App() {
         <Login />
         <PlayGround />
 
-        <div className='bg-circle bg-circle__blue -top-[25rem] -left-[25rem] w-[50rem]'></div>
-        <div className='bg-circle bg-circle__green top-[10rem] left-[10rem] w-[20rem] '></div>
-        <div className='bg-circle bg-circle__blue top-[25rem] -right-[25rem] w-[50rem]'></div>
-        <div className='bg-circle bg-circle__green top-[10rem] right-[10rem] w-[20rem]'></div>
+        <div className='bg-circle bg-circle__blue -top-[15rem] -left-[15rem] w-[30rem] h-[30rem] sm:-top-[25rem] sm:-left-[25rem] sm:w-[50rem] sm:h-[50rem]'></div>
+        <div className='bg-circle bg-circle__green top-[5rem] left-[5rem] w-[10rem] h-[10rem] sm:top-[10rem] sm:left-[10rem] sm:w-[20rem] sm:h-[20rem]'></div>
+        <div className='bg-circle bg-circle__blue top-[15rem] -right-[15rem] w-[30rem] h-[30rem] sm:top-[25rem] sm:-right-[25rem] sm:w-[50rem] sm:h-[50rem]'></div>
+        <div className='bg-circle bg-circle__green top-[25rem] right-[5rem] w-[10rem] h-[10rem] sm:top-[10rem] sm:right-[10rem] sm:w-[20rem] sm:h-[20rem]'></div>
       </div>
     </UserProvider>
 
